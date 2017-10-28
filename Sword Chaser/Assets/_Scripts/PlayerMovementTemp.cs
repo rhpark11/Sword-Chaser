52c053d80a1528fedfee24a3b8213dda57119de7
Blake Wakasa <bwakasa@uci.edu>
2017-10-28 13:48:12 -0700

    Change scene made temp playermovement script

--- Sword Chaser/Assets/_Scripts/PlayerMovementTemp.cs
+++ Sword Chaser/Assets/_Scripts/PlayerMovementTemp.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class PlayerMovementTemp : MonoBehaviour {
 
+    public float speed;
+    private Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
-		
+        rb = gameObject.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKey("a"))
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+        else if (Input.GetKey("d"))
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
 	}
 }
