64eb7bd867a17b0d53019157abdd20385f5c644a
CraftServices <33108370+CraftServices@users.noreply.github.com>
2017-11-01 15:54:19 -0700

    adding scene
    add jons scene add standard assets and scripts

---
+++ Sword Chaser/Assets/_Scripts/PowerUpScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpScript : MonoBehaviour {
+
+    HUDscript hud;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            hud = GameObject.Find("Main Camera").GetComponent<HUDscript>();
+            hud.IncreaseScore(100);
+            Destroy(this.gameObject);
+        }
+    }
+}
