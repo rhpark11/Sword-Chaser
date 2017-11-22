1131809ee15ae77ace0a07e026533462d060dbe0
CraftServices <33108370+CraftServices@users.noreply.github.com>
2017-11-21 22:28:08 -0800

    added soundFX for coin pickup
    new script on player to cue soundFX

--- Sword Chaser/Assets/_Scripts/PowerUpScript.cs
+++ Sword Chaser/Assets/_Scripts/PowerUpScript.cs
@@ -7,21 +7,30 @@ public class PowerUpScript : MonoBehaviour {
     //HUDscript hud;
     public AudioSource soundSource;
     public AudioClip sound;
-    private bool hasPlayed = false;
+    //private bool hasPlayed = false;
 
     void Start()
     {
         //tickSource = GetComponent<AudioSource>();
+        //soundSource = GetComponent<AudioSource>();
     }
-
+    
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
             //hud = GameObject.Find("Main Camera").GetComponent<HUDscript>();
             //hud.IncreaseScore(100);
-            soundSource.PlayOneShot(sound);
-            Destroy(this.gameObject);
+
+            //soundSource.GetComponent<AudioListener>().enabled = true;
+            //soundSource.PlayOneShot(sound);
+
+            soundSource.GetComponent<AudioSource>().PlayOneShot(sound);
+            //AudioSource.PlayClipAtPoint(sound, transform.position);
+            //yield return new WaitForSeconds(0.4f);  //only works in a co-routine
+            //soundSource.Play();
+            Destroy(this.gameObject);//destorys the audioSource so can't play the AudioClip
         }
     }
+    
 }
---
+++ Sword Chaser/Assets/_Scripts/SoundFX.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundFX : MonoBehaviour {
+
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+	
+	void OnTriggerEnter2D(Collider2D other) {
+
+        if (other.tag == "coin_19")
+        {
+            audioSource.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioClip);
+            Destroy(other.gameObject);
+        }
+	}
+}
