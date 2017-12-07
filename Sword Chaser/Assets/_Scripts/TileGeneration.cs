4ff1c058e1598d8f00910393c011ecef3783e309
Blake Wakasa <bwakasa@uci.edu>
2017-12-06 16:04:22 -0800

    Removing bad generation script/changing plat prefabs

--- Sword Chaser/Assets/_Scripts/TileGeneration.cs
+++ Sword Chaser/Assets/_Scripts/TileGeneration.cs
@@ -8,7 +8,9 @@ public class TileGeneration : MonoBehaviour {
     public float distBetweenMin;
     public float distBetweenMax;
     private float distBetween;
+    private float distHeight;
     public float maxHeight;
+    public float minHeight;
     private float platWidth;
     private int platSelection;
     // Use this for initialization
@@ -17,13 +19,15 @@ public class TileGeneration : MonoBehaviour {
 	void Start () {
         
         platWidth = plat.GetComponent<BoxCollider2D>().size.x;
-	}
+        Instantiate(platformsA[platSelection], transform.position, transform.rotation);
+    }
 	
 	// Update is called once per frame
 	void Update () {
          if (transform.position.x <genPoint.position.x)
         {
             distBetween = Random.Range(distBetweenMin, distBetweenMax);
+            distHeight = Random.Range(minHeight, maxHeight);
 
             transform.position = new Vector3(transform.position.x + platWidth + distBetween,
                 transform.position.y, transform.position.z);
