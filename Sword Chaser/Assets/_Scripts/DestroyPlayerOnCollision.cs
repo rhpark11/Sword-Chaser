fd351179deda6be98d2c402d2f2a7180447d8ecf
CraftServices <33108370+CraftServices@users.noreply.github.com>
2017-12-06 22:13:19 -0800

    add enemy scripts
    player gets destroyed on collision with enemy objects. Red enemy now jumps

---
+++ Sword Chaser/Assets/_Scripts/DestroyPlayerOnCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyPlayerOnCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Destroy(gameObject, 3f); destroy after a delay
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Destroy(collision.gameObject, 0.1f);
+            //call the game over script because player object has been destroyed
+        }
+    }
+}
---
+++ Sword Chaser/Assets/_Scripts/enemyJump.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyJump : MonoBehaviour {
+
+    public float jumpForce = 500f;
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    Rigidbody2D ej;
+
+    // Use this for initialization
+    void Start () {
+        ej = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+        if (grounded)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));//make the enemy go up and then drop back down
+        }
+        
+    }
+}
