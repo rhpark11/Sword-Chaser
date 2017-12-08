227c44b70c60fae56f5ef15ac8cdb6ef3834cad0
CraftServices <33108370+CraftServices@users.noreply.github.com>
2017-12-07 16:25:34 -0800

    add faker enemy
    faker enemy jumps when the player is near. still needs a destroy on contact

---
+++ Sword Chaser/Assets/EnemyFaker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFaker : MonoBehaviour {
+
+    public float jumpForce = 100f;
+    
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+
+    // Use this for initialization
+    void Start () {
+        grounded = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("in on trigger");
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        
+        if (grounded && collision.tag == "Player")
+        {
+            Debug.Log("in if grounded");
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+        }
+        
+    }
+}
