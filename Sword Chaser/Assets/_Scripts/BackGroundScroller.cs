d53f532249530cc7e0515a0b1097162edd1eb745
Darren <dartren00@gmail.com>
2018-02-17 17:58:43 -0800

    Added scrolling to background
    Parameters are tight cause the background is scaled. Still have some bugs to fix.

---
+++ Sword Chaser/Assets/_Scripts/BackGroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundScroller : MonoBehaviour {
+
+    public float scrollSpeed;
+    public float tileSizeX;
+
+    private Vector2 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeX);
+        transform.position = startPosition + Vector2.right * newPosition;
+    }
+}
